3a0bb3f1060a3c64afaf5f9df1dc0e1c3c2d624f
Diony S. Castro <dnycstr@gmail.com>
2025-05-06 20:41:51 +0800

    Update to net8

--- src/RedFalcon.API/Controllers/OrganizationsController.cs
+++ src/RedFalcon.API/Controllers/OrganizationsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using RedFalcon.Application.DTOs;
 using RedFalcon.Application.Interfaces.Services;
 using RedFalcon.Application.ResourceParameters;
